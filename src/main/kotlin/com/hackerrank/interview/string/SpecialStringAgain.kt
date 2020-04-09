aad0b2fae0f396cd967647c665cfc1c54bac25d7
El gran <ferran@elgran.cat>
2020-04-09 02:02:56 +0200

    Special String Again

--- src/main/kotlin/com/hackerrank/interview/string/SpecialStringAgain.kt
+++ src/main/kotlin/com/hackerrank/interview/string/SpecialStringAgain.kt
@@ -5,25 +5,58 @@ import java.util.*
 fun isSpecial(s: String): Boolean {
     val first: Char = s.toList().first()
     s.indices.forEach { i ->
-        if (s[i] != first && (s.length == 2 || i != s.length / 2)) {
+        val isMiddle = i == s.length / 2 && s.length % 2 == 1
+        if (s[i] != first && !isMiddle) {
             return false
         }
     }
     return true
 }
 
-// Complete the substrCount function below.
-fun substrCount(s: String): Long {
+fun substrCountSlow(s: String): Long {
     var count = 0L
     s.indices.forEach { i ->
-        (i + 2 .. s.length).forEach { subI ->
-//            println("$i:: $subI :> ${s.substring(i, subI)} == ${isSpecial(s.substring(i, subI))}")
-            if(isSpecial(s.substring(i, subI))) {
+        (i + 2..s.length).forEach { subI ->
+            if (isSpecial(s.substring(i, subI))) {
                 count++
             }
         }
     }
     return count + s.length
+}
+
+fun String.longestEqual(c: Char): Int {
+    return fold(0 to true) { acc, v ->
+        if (v == c && acc.second) {
+            acc.first + 1 to true
+        } else {
+            acc.first to false
+        }
+    }.first
+}
+
+// Complete the substrCount function below.
+fun substrCount(s: String): Long {
+    var count = 1L
+    var previousLetter: Char = s.first()
+    var sameLetterCount = 1
+    (1 until s.length).forEach { i ->
+        if (s[i] != previousLetter) {
+            val subStrInit = i + 1
+            val subStrEnd = s.length.coerceAtMost(subStrInit + sameLetterCount)
+            val subStr = s.substring(subStrInit, subStrEnd)
+            val subCount = subStr.longestEqual(previousLetter)
+            if (subCount in 1..sameLetterCount) {
+                count += subCount
+            }
+            previousLetter = s[i]
+            sameLetterCount = 1
+        } else {
+            ++sameLetterCount
+        }
+        count += sameLetterCount
+    }
+    return count
 }
 
 fun main(args: Array<String>) {
--- src/test/groovy/com/hackerrank/interview/hashmaps/SpecialStringAgain.groovy
+++ src/test/groovy/com/hackerrank/interview/hashmaps/SpecialStringAgain.groovy
@@ -1,6 +1,6 @@
 package com.hackerrank.interview.hashmaps
 
-import com.hackerrank.interview.string.SherlockAndTheValidStringKt
+
 import com.hackerrank.interview.string.SpecialStringAgainKt
 import spock.lang.Specification
 import spock.lang.Unroll
@@ -20,6 +20,43 @@ class SpecialStringAgain extends Specification {
         "aaaa"    || 10
     }
 
+    def "between slow and fast"() {
+        expect:
+        SpecialStringAgainKt.substrCount(input) == SpecialStringAgainKt.substrCountSlow(input)
+
+        where:
+        input || nothing
+        "ababcbcccbccbcacccbababbcacaaaaacbabcabcacaccabaabcaaaacacbccccaaccbcbccaccacbcaaababaacbccbbbabccbaabab" || true
+        "ccacacabccacabaaaabbcbccbabcbbcaccabaababcbcacabcabacbbbccccabcbcabbaaaaabacbcbbbcababaabcbbaaababababb"  || true
+        "abcaabcaacacbbaccbbabbcbbcbacbacabaaaaccacbaabccabbacabaabaaaabbccbaaaabacabcacbbabbacbcbccbbbaaabaaaca"  || true
+        "abacccaacbcccaacbbcaabcbbccbccacbbcbcaaabbaababacccbacacbcbcbbccaacbbacbcbaaaacaccbcaaacbbcbbabaaacbacc"  || true
+        "accbbabbcccbcbcbcbcabbccbacccbacabcaacbcaccabbacbbccccaabbacccaacbbbacbccbcaaaaaabaacaaabccbbcccaacbacbc" || true
+        "caaacaacaaaacbbaaccacbcbaaaccaabcbccacaaccccacaacbcacccbcababcabacaabbcacccbacbbaaaccabbabaaccabbcbbcaab" || true
+        "bcabaacabacbcabbaaabccabcacbcbabcbccbabcabbbcbacaaacaabbbabbaacbbacaccccabbabcbcabababbcbaaacbaacbacacba" || true
+        "bbcacccbccbbbcbcabcabbbcaabbaccccabaabbcbcccabaacccccaaacbbbcbcacacbabaccccbcbabacaaaabcccaaccacbcbbccca" || true
+        "acccbbcaaaccccaabacabcabbccaababbcabccbcaccccbaaabbbcbabaccacaabcabcbacaccbaccbbaabccbbbccaccabccbabbbcc" || true
+        "baabcaabcabcbbabccbaaccabaacbbaaaabcbcabaacacbcaabbaaabaaccacbaacababcbacbaacacccacaacbacbbaacbcbbbabccb" || true
+        "ababcbcccbccbcacccbababbcacaaaaacbabcabcacaccabaabcaaaacacbccccaaccbcbccaccacbcaaababaacbccbbbabccbaabab" || true
+
+    }
+
+    def "compare old and new"() {
+        expect:
+        def input = "ccccabc" //bacbcbbbcababaabcbbaaababababb"
+
+        SpecialStringAgainKt.substrCount(input) == SpecialStringAgainKt.substrCountSlow(input)
+    }
+
+    def "longest equal"() {
+        expect:
+        SpecialStringAgainKt.longestEqual(input, letter as Character) == result
+
+        where:
+        input | letter || result
+        "aaa" | 'a'     | 3
+        "aba" | 'a'     | 1
+        "aba" | 'b'     | 0
+    }
 
     @Unroll
     def "is #input special"() {
@@ -30,7 +67,7 @@ class SpecialStringAgain extends Specification {
         where:
         input   || result
         "asa"   || true
-        "aasa"  || true
+        "aasa"  || false
         "asasd" || false
         "as"    || false
     }
