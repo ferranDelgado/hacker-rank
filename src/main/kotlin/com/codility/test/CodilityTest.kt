833f41322de66d753b791e99dbf2f864a19bd84b
Ferran.Delgado <ferran5101990@gmail.com>
2020-04-20 17:33:23 +0200

    Codility

---
+++ src/main/kotlin/com/codility/test/CodilityTest.kt
@@ -0,0 +1,24 @@
+package com.codility.test
+
+fun forTest(a: List<Int>) = solution(a.toIntArray())
+
+fun solution(A: IntArray): Int {
+    var min = 0
+    A.sort()
+    val filter = A.filter { it > 0 }
+    if(filter.isEmpty() || filter.first() != 1) {
+        return 1
+    } else {
+        filter.indices.forEach {
+            val diff = if (it + 1 < filter.size) A[it + 1] - A[it] else 0
+
+
+            if (diff > 1 && filter[it] >= 0) {
+                return filter[it] + 1
+            } else if (filter[it] >= 0) {
+                min = filter[it]
+            }
+        }
+        return min + 1
+    }
+}
---
+++ src/test/groovy/com/codility/test/CodelyTest.groovy
@@ -0,0 +1,50 @@
+package com.codility.test
+
+import com.codility.test.CodilityTestKt
+import spock.lang.Specification
+
+class CodelyTest extends Specification {
+
+    def "Sample 0"() {
+        given:
+        def input = [1, 3, 6, 4, 1, 2]
+
+        expect:
+        CodilityTestKt.forTest(input) == 5
+
+    }
+
+    def "negative numbers"() {
+        given:
+        def input = [-1, -3]
+
+        expect:
+        CodilityTestKt.forTest(input) == 1
+    }
+
+
+    def "negative numbers and more"() {
+        given:
+        def input = [-1, -3, 1]
+
+        expect:
+        CodilityTestKt.forTest(input) == 2
+    }
+
+    def "negative numbers and more more"() {
+        given:
+        def input = [-1, -3, 1, 2]
+
+        expect:
+        CodilityTestKt.forTest(input) == 2
+    }
+
+    def "1 but not the first"() {
+        given:
+        def input = [-1, -3, 2, 4, 5, 6, 9]
+
+        expect:
+        CodilityTestKt.forTest(input) == 1
+    }
+
+}
