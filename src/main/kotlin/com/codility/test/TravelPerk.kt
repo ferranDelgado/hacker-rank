071cfaa2514f71664002cc329b4333f93980f9dd
Ferran.Delgado <ferran5101990@gmail.com>
2020-04-21 02:05:08 +0200

    TP test done

--- src/main/kotlin/com/codility/test/TravelPerk.kt
+++ src/main/kotlin/com/codility/test/TravelPerk.kt
@@ -2,141 +2,94 @@ package com.codility.test
 
 fun forTest(a: List<Int>) = solution(a.toIntArray())
 
-fun buildTree(blockSize: Int, B: List<Int>) {
-    println("$B: ${B.sum()}")
-    println("First path: ${B.slice(0 until 2)}")
-    (0 until blockSize).forEach { firstCut ->
-        val part = B.slice(0 until firstCut) + B.slice(firstCut + 1 until B.size)
-        println("[$firstCut] path: ${part.slice((0 until 2))}")
-        B.indices.filter { it != firstCut }.forEach { secondCut ->
-            val partB = part.slice(0 until secondCut) + part.slice(secondCut + 1 until part.size)
-            println("[$firstCut][$secondCut] path: ${partB.slice((0 until 2))}")
-        }
-    }
-}
 
 fun solution(A: IntArray): Boolean {
-    val deletions = 2
-    val blockSize = (A.size - deletions) / 3
-    println("blockSize: $blockSize")
-    val part = A.slice(0 until (blockSize + deletions))
-    buildTree(blockSize, part)
-    return false
+    val workersSize = (A.size - 2) / 3
+
+    return listOf(
+        noCuts(A.toList(), workersSize),
+        oneCuts(A.toList(), workersSize),
+        twoCuts(A.toList(), workersSize)
+    ).find { it() }?.let { true } ?: false
 }
 
-fun <E> List<E>.delete(positionA: Int, positionB: Int, blockSize: Int = 2): List<E> {
-    val leftCut = positionA.coerceAtMost(positionB)
-    val rightCut = positionA.coerceAtLeast(positionB)
-    val left = subList(0, leftCut)
-    val middle = subList(listOf(size, leftCut + 1, rightCut).min()!!, rightCut)
-    val right = subList(size.coerceAtMost(rightCut + 1), size)
-    return (left + middle + right).slice(0 until size.coerceAtMost(blockSize))
+fun twoCuts(list: List<Int>, workersSize: Int): () -> Boolean = {
+    val leftTo = workersSize + 2
+    val left = list.subList(0, leftTo)
+    val middleTo: Int = leftTo + workersSize
+    val middle = list.subList(leftTo, middleTo)
+    val right = list.subList(middleTo, list.size)
+    middle.sum() == right.sum() && left.delete2MustSum(middle.sum())
 }
 
-enum class CutType {
-    CUT_ONE, CUT_TWO, CUT_NONE
-}
+fun oneCuts(list: List<Int>, workersSize: Int): () -> Boolean = {
+    val leftTo = workersSize + 1
+    val left = list.subList(0, leftTo)
+    val opt1 = {
+        //middle has one cut
+        val middleTo = leftTo + workersSize + 1
+        val middle = list.subList(leftTo, middleTo)
+        val right = list.subList(middleTo, list.size)
 
-fun lastFreePos(size: Int, a: Int, b: Int): CutType {
-    return when {
-        a + b == (size * 2) - 1 -> {
-            CutType.CUT_NONE
-        }
-        listOf(a, b).contains(size - 1) -> {
-            CutType.CUT_ONE
-        } else -> {
-            CutType.CUT_TWO
-        }
+        val rightSum = right.sum()
+        left.delete1MustSum(rightSum) && middle.delete1MustSum(rightSum)
     }
+
+    val opt2 = {
+        //right has one cut
+        val middleTo = leftTo + workersSize
+        val middle = list.subList(leftTo, middleTo)
+        val right = list.subList(middleTo, list.size)
+
+        val middleSum = middle.sum()
+        left.delete1MustSum(middleSum) && right.delete1MustSum(middleSum)
+    }
+    listOf(opt1, opt2).find { it() }?.let { true } ?: false
 }
 
-/**
- * Last cut aka worker 3
- */
-fun deleteOneOrNone(list: List<Int>, from: Int, to: Int, blockSize: Int, previousSum: Int? = null, cutsLeft: Int): Boolean {
-    val sum = list.sum()
-    (list.indices).forEach { first ->
-        if(sum - list[first] == previousSum) {
-            return true
-        }
+fun List<Int>.delete1MustSum(sum: Int): Boolean {
+    val sumToCut = sum()
+    return find { sumToCut - it == sum }?.let { true } ?: false
+}
+
+fun List<Int>.delete2MustSum(sum: Int): Boolean {
+    if (sum < 0) {
         return false
     }
-
-    return false
+    val set = mutableSetOf<Int>()
+    return find {
+        if (set.contains(it)) {
+            true
+        } else {
+            set.add(sum - it)
+            false
+        }
+    }?.let { true } ?: false
 }
 
-fun deleteTwoOrNone(list: List<Int>, from: Int, to: Int, blockSize: Int, previousSum: Int? = null, cutsLeft: Int): Boolean {
-    if (cutsLeft == 0) {
-        return true
-    } else {
-        val sublist = list.slice((0 until list.size.coerceAtMost(blockSize + 2)))
-        println("Next tree on $sublist")
-        val sum = sublist.sum()
-        (0 until blockSize).forEach { first ->
-            sublist.indices.forEach { second ->
-                if (first != second) {
-                    val optB = sublist.delete(first, second, blockSize)
-                    val sumB = sum - sublist[first] - sublist[second]
-                    if (previousSum == sumB) {
-                        println("Next -- Delete position $first and $second: $optB: $sumB")
-                        when(lastFreePos(sublist.size, first, second)) {
-                            CutType.CUT_ONE -> {
-                                TODO()
-                            }
-                            CutType.CUT_TWO -> {
-                                TODO()
-                            }
-                            CutType.CUT_NONE -> {
-                                TODO()
-                            }
-                        }
-//                        println("Next -- Delete position $first and $second: $optB: $sumB")
-//                        val nextInit = lastFreePos(sublist.size, first, second)
-//                        val nextList = list.subList(nextInit, list.size)
-//                        println("nextList = $nextList")
-//                        deleteTwoOrNone(
-//                            nextList,
-//                            0,
-//                            blockSize + 2,
-//                            blockSize,
-//                            sumB,
-//                            cutsLeft - 1
-//                        )
-                    }
-                    println("-- Delete position $first and $second: $optB: $sumB")
-                }
-            }
-        }
+fun noCuts(list: List<Int>, workersSize: Int): () -> Boolean = {
+    val workTime = list.subList(0, workersSize).sum()
+    //Middle has 0 cut
+    val opt1 = {
+        val middle = list.subList(workersSize, workersSize * 2)
+        val right = list.subList(workersSize * 2, list.size)
+        workTime == middle.sum() && right.delete2MustSum(right.sum() - workTime)
     }
 
-    return false
-}
+    //Middle has 1 cut
+    val opt2 = {
+        val middle = list.subList(workersSize, (workersSize * 2) + 1)
+        val right = list.subList((workersSize * 2) + 1, list.size)
+        middle.delete1MustSum(workTime) && right.delete1MustSum(workTime)
+    }
 
-fun main() {
-    val size = 4
-    val blockSize = 2
-    val deletions = 2
-    val list = listOf(1, 3, 4, 2, 2, 2, 1, 1, 2)
-    println("List: $list")
-    val sum = list.sum()
-    println("Do not delete: ${list.slice((0 until blockSize))}: ${list.slice((0 until blockSize)).sum()}")
-    deleteTwoOrNone(
-        list.subList(2, list.size),
-//        list.subList(2, 2 + blockSize + deletions),
-        0,
-        blockSize + deletions,
-        blockSize,
-        list.slice((0 until blockSize)).sum(),
-        2
-    )
 
-//    (0 until blockSize).forEach { first ->
-//        (0 until size).forEach { second ->
-//            if(first != second) {
-//                val optB = list.delete(first, second)
-//                val sumB = sum - list[first] - list[second]
-//                println("Delete position $first and $second: $optB: $sumB")
-//            }
-//        }
-//    }
+    //Middle has 2 cut
+    val opt3 = {
+        val middle = list.subList(workersSize, workersSize * 2 + 2)
+        val right = list.subList(workersSize * 2 + 2, list.size)
+        workTime == right.sum() && middle.delete2MustSum(middle.sum() - workTime)
+    }
+
+    listOf(opt1, opt2, opt3).find { it() }?.let { true } ?: false
 }
--- src/test/groovy/com/codility/test/TravelPerk.groovy
+++ src/test/groovy/com/codility/test/TravelPerk.groovy
@@ -2,6 +2,7 @@ package com.codility.test
 
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class TravelPerk extends Specification {
 
@@ -14,4 +15,54 @@ class TravelPerk extends Specification {
 
     }
 
+    @Unroll
+    def "where #input"() {
+        expect:
+        TravelPerkKt.forTest(input) == result
+
+        where:
+        input                       || result
+        [1, 3, 4, 2, 2, 2, 1, 1, 2] || true
+        [1, 1, 1, 1, 1, 1]          || false
+        [1, 1, 1, 0, 1, 2, 3, 0, 0] || true
+        [1, 2, 2, 2, 1, 2, 4, 0, 0] || true
+        [1, 2, 2, 4, 0, 0, 2, 1, 2] || true
+        [0, 0, 3, 3, 6, 0, 6, 0]    || true
+    }
+
+    def "out of index"() {
+        given:
+        def input = [1, 1, 1, 1, 1]
+
+        expect:
+        TravelPerkKt.forTest(input)
+
+    }
+
+    def "Long test"() {
+        given:
+        def input = [1, 2] * 20_000
+
+        println input
+        expect:
+        !TravelPerkKt.forTest(input)
+    }
+
+    def "delete 1 must sum"() {
+        given:
+        def input = [1, 2, 4, 1]
+
+        expect:
+        TravelPerkKt.delete1MustSum(input, 4)
+
+    }
+
+    def "delete 2 must sum"() {
+        given:
+        def input = [1, 1]
+
+        expect:
+        TravelPerkKt.delete2MustSum(input, 2)
+
+    }
 }
